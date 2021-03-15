fc7dad88f8f8f877991f2914e5170a4b36ae51b6
James Vodanovich <james@jvodan.com>
2021-03-15 11:26:14 +1100

    set perms on pdns db

--- commissioning/late/root/setup.sh
+++ commissioning/late/root/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  sqlite3 /var/lib/powerdns/pdns.sqlite3 < /usr/share/pdns-backend-sqlite3/schema/schema.sqlite3.sql
-
-
+ chown -R pdns /var/lib/powerdns/
+ chmod og-r /var/lib/powerdns/pdns.sqlite3 
