40217b9ba21feeb1959715f853ce4b78a375cbb9
Mark Ratjens <mark@ratjens.com>
2022-04-06 12:55:06 +1000

    Delete setup.sh

--- commissioning/late/root/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
- sqlite3 /var/lib/powerdns/pdns.sqlite3 < /usr/share/pdns-backend-sqlite3/schema/schema.sqlite3.sql
- chown -R pdns /var/lib/powerdns/
- chmod og-r /var/lib/powerdns/pdns.sqlite3 
- pdnsutil create-zone spaces.internal ns1.spaces.internal
